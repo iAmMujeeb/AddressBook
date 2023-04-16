ef1e9f203e58a27145a12cf26fd33f675c06417d
Your Name <saifi.mujeeb2@gmail.com>
2023-04-16 13:13:52 +0530

    Added: Create a contact in address book

---
+++ src/AddressBookProblem/Contact.java
@@ -0,0 +1,12 @@
+package AddressBookProblem;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private int phN;
+    private String emailId;
+}
