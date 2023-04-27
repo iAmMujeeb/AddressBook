51c8babbd63cf2d70be883711b9cef07490720b8
Your Name <saifi.mujeeb2@gmail.com>
2023-04-27 10:55:39 +0530

    Added: ability to add multiple persons in an addressbook

--- src/AddressBookProblem/AddressBook.java
+++ src/AddressBookProblem/AddressBook.java
@@ -1,12 +1,15 @@
 package AddressBookProblem;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
 
-    Contact contact = new Contact();
+
+    ArrayList<Contact> personList = new ArrayList<>();
 
     public void addContact() {
+        Contact contact = new Contact();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter FN");
         String firstName = scanner.next();
@@ -32,70 +35,6 @@ public class AddressBook {
         contact.setZip(zip);
         contact.setPhN(phN);
         contact.setEmailId(emailId);
-        System.out.println(contact);
+        personList.add(contact);
     }
-
-    public int editPerson(int temp1) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println();
-        switch (temp1) {
-            case 1:
-                System.out.println("Enter FN");
-                String firstName = scanner.next();
-                contact.setFirstName(firstName);
-                break;
-            case 2:
-                System.out.println("Enter LN");
-                String lastName = scanner.next();
-                contact.setLastName(lastName);
-                break;
-            case 3:
-                System.out.println("Enter Address");
-                String address = scanner.next();
-                contact.setCity(address);
-                break;
-            case 4:
-                System.out.println("Enter City");
-                String city = scanner.next();
-                contact.setCity(city);
-                break;
-            case 5:
-                System.out.println("Enter State");
-                String state = scanner.next();
-                contact.setState(state);
-                break;
-            case 6:
-                System.out.println("Enter Zip Code");
-                int zip = scanner.nextInt();
-                contact.setZip(zip);
-                break;
-            case 7:
-                System.out.println("Enter Phone Number");
-                Long phN = scanner.nextLong();
-                contact.setPhN(phN);
-                break;
-            case 8:
-                System.out.println("Enter Email Id");
-                String emailId = scanner.next();
-                contact.setEmailId(emailId);
-                break;
-            default:
-                System.out.println("Wrong Number");
-        }
-        System.out.println(contact);
-        return temp1;
-    }
-
-    public void deletePerson() {
-        contact.setCity("");
-        contact.setLastName("");
-        contact.setAddress("");
-        contact.setCity("");
-        contact.setState("");
-        contact.setZip(0);
-        contact.setPhN(0);
-        contact.setEmailId("");
-        System.out.println(contact);
-    }
-
 }
--- src/AddressBookProblem/AddressBookMain.java
+++ src/AddressBookProblem/AddressBookMain.java
@@ -1,39 +1,28 @@
 package AddressBookProblem;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class AddressBookMain {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Address Book Program");
+        ArrayList<AddressBook> addressBook = new ArrayList<>();
         AddressBook addressBook1 = new AddressBook();
-        System.out.println("Enter Your Options:");
-        System.out.println("1.Add a Contact");
-        System.out.println("2.Edit a Contact");
-        System.out.println("3.Delete a Contact");
-        Scanner scanner = new Scanner(System.in);
-        int options = scanner.nextInt();
-        switch (options){
-            case 1:
-                addressBook1.addContact();
-                break;
-            case 2:
-                System.out.println("Enter Your Options:");
-                System.out.println("1.First Name");
-                System.out.println("2.Last Name");
-                System.out.println("3.Address");
-                System.out.println("4.City");
-                System.out.println("5.State");
-                System.out.println("6.Zip");
-                System.out.println("7.Phone Number");
-                System.out.println("8.Email Id");
-                int temp = scanner.nextInt();
-                addressBook1.editPerson(temp);
-            case 3:
-                addressBook1.deletePerson();
-                break;
-            default:
-                System.out.println("Wrong Number");
-        }
+        addressBook1.addContact();
+        addressBook1.addContact();
+        addressBook1.addContact();
+        addressBook.add(addressBook1);
+
+        AddressBook addressBook2 = new AddressBook();
+        addressBook2.addContact();
+        addressBook2.addContact();
+        addressBook2.addContact();
+        addressBook.add(addressBook2);
+        
+        AddressBook addressBook3 = new AddressBook();
+        addressBook3.addContact();
+        addressBook3.addContact();
+        addressBook3.addContact();
+        addressBook.add(addressBook3);
     }
 }
